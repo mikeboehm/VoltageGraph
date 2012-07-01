78b9d6a79b361e3b374b04265cd939d49cb24488
Mike Boehm <mike@MikeBook-Pro.local>
2012-07-01 17:56:23 +1000

    Reading voltage from one battery every second

--- VoltageGraph.ino
+++ VoltageGraph.ino
@@ -1,1 +1,21 @@
+/*
+  VoltageGraph
+  Maps the voltage over time
+*/
 
+const float referenceVolts = 5.0;
+const int batteryPin = 0;
+int counter = 0;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int val = analogRead(batteryPin);
+  float volts = (val / 1023.0) * referenceVolts;
+  Serial.println(volts);
+//  Serial.println(counter);
+  delay(1000);
+  counter = counter + 1;
+}
