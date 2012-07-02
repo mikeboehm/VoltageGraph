31d128ac3fa9b499a31db954b9291bfd0153dae5
Mike Boehm <mike@MikeBook-Pro.local>
2012-07-03 09:51:15 +1000

    Map two batteries over time

--- VoltageGraph.ino
+++ VoltageGraph.ino
@@ -4,18 +4,27 @@
 */
 
 const float referenceVolts = 5.0;
-const int batteryPin = 0;
-int counter = 0;
+const int batteryPin1 = 0;
+const int batteryPin2 = 1;
+unsigned long time;
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  int val = analogRead(batteryPin);
-  float volts = (val / 1023.0) * referenceVolts;
-  Serial.println(volts);
-//  Serial.println(counter);
-  delay(1000);
-  counter = counter + 1;
+  int val1 = analogRead(batteryPin1);
+  int val2 = analogRead(batteryPin2);
+
+  float volts1 = (val1 / 1023.0) * referenceVolts;
+  float volts2 = (val2 / 1023.0) * referenceVolts;
+
+  time = millis();
+  Serial.print(time/1000); // Number of seconds that have elapsed
+  Serial.print(',');
+  Serial.print(volts1);
+  Serial.print(',');
+  Serial.println(volts2);
+
+  delay(60000); // One minute delay
 }
